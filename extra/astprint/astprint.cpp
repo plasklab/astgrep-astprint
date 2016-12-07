a8a84304aadd7596d67eef75b320e0ae97b51204
ayato1995 <ayato.k070206@outlook.jp>
2016-12-07 20:00:05 +0900

    VisitContinueStmtの実装

--- extra/astprint/astprint.cpp
+++ extra/astprint/astprint.cpp
@@ -2345,10 +2345,15 @@ public:
   }
   // ContinueStmt
   bool VisitContinueStmt(ContinueStmt *Continue) {
-    llvm::outs() << "{:kind \"Continue\"";
+    /*llvm::outs() << "{:kind \"Continue\"";
     checkLabel(); 
     PrintSourceRange(Continue->getSourceRange());
     llvm::outs() << "}";
+    */
+    Node t = PrintSourceRange(Continue->getSourceRange());
+    ContinueStatement *CS = new ContinueStatement(t);
+    Node *np = CS;
+    prog.push_back(np);
     return true;
   } 
   
